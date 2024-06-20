ff76ead911ba3427d954d9f3ca183adfb24c83dd
Denis Moini <denismoini09@gmail.com>
2024-06-20 05:26:47 +0300

    player perfoemance 1

--- src/screens/PlayerScreen.jsx
+++ src/screens/PlayerScreen.jsx
@@ -4,6 +4,7 @@ import { usePlayer } from "../PlayerContext"
 import { useNavigate } from "react-router-dom"
 import { Row, Col } from "react-bootstrap"
 import axios from "axios"
+import { BarChart, Bar, Rectangle, Legend, CartesianGrid, XAxis, YAxis, Tooltip, ResponsiveContainer } from 'recharts'
 const PlayerScreen = () => {
 
   const [data, setData] = useState([])
@@ -20,7 +21,7 @@ const PlayerScreen = () => {
         const response = await axios
           .get(`https://corsproxy.io/?https://fantasy.premierleague.com/api/element-summary/${playerId}/`)
         const data = await response.data
-        //console.log(data)
+        console.log(data)
         setData(data)
       } catch (error) {
         const errMsg = error?.response?.data?.msg || error?.message
@@ -30,7 +31,23 @@ const PlayerScreen = () => {
     }
     playerData()
   }, [playerId])
-  console.log(player)
+  //console.log(player)
+
+  const { history } = data
+  console.log(history)
+
+  const returnPoints = () => {
+    const data = []
+    history?.map(x => x.total_points)?.forEach((x, key) => {
+      const subData = {name: key+1, points: x}
+      data.push(subData)
+    })
+
+    return data
+  }
+
+  const points = returnPoints()
+  console.log(points)
 
   if (players.length === 0) return <h1>Loading...</h1>
   if (!player) navigate('/statistics/players/1')
@@ -42,7 +59,22 @@ const PlayerScreen = () => {
         <h6>{elementType}</h6>
       </div>
       {error === 'Network Error' && <div>Check Your internet Connection!</div>}
-      {!!(error === '') && <div>You On</div>}
+      {!!(error === '') && <div>
+        <div className="chart">
+        <div className='chart-heading'>Player Performance</div>
+        <ResponsiveContainer width="100%" height="100%">
+        <BarChart width={500} height={400} data={points}
+        margin={{top: 5, right: 30, left: 20, bottom: 5}}>
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis dataKey="name" />
+            <YAxis />
+            <Tooltip />
+            <Legend />
+            <Bar dataKey="points" fill='#57d557' activeBar={<Rectangle fill='gold' stroke='purple' />} />
+        </BarChart>
+        </ResponsiveContainer>
+        </div>
+        </div>}
     </>
   )
 }
