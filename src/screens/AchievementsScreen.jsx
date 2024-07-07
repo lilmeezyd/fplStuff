efd7c4820687abc159e26de2c68a04f94ffec798
Denis Moini <denismoini09@gmail.com>
2024-07-07 16:32:32 +0300

    modal captaincy

--- src/screens/AchievementsScreen.jsx
+++ src/screens/AchievementsScreen.jsx
@@ -23,6 +23,29 @@ const AchievementsScreen = () => {
         value: 10
       }
     }
+
+    if(action.type === 'captain_by_16') {
+      return {
+        value: 16
+      }
+    }
+
+    if(action.type === 'captain_by_20') {
+      return {
+        value: 20
+      }
+    }
+    if(action.type === 'captain_by_30') {
+      return {
+        value: 30
+      }
+    }
+
+    if(action.type === 'captain_by_35') {
+      return {
+        value: 35
+      }
+    }
   }
   const [ state, dispatch ] = useReducer(reducer, { value: 0})
   const { value } = state
@@ -72,6 +95,26 @@ const AchievementsScreen = () => {
     dispatch({type: 'captain_by_10'})
   }
 
+  const handleSixteen = () => {
+    setShow(true)
+    dispatch({type: 'captain_by_16'})
+  }
+
+  const handleTwenty = () => {
+    setShow(true)
+    dispatch({type: 'captain_by_20'})
+  }
+
+  const handleThirty = () => {
+    setShow(true)
+    dispatch({type: 'captain_by_30'})
+  }
+
+  const handleThirtyFive = () => {
+    setShow(true)
+    dispatch({type: 'captain_by_35'})
+  }
+
   //console.log(playerStats.find(player => player.history[0].element === 20)?.history.find(x => x.round === 3))
 
   const getCaptains = useCallback(
@@ -80,6 +123,7 @@ const AchievementsScreen = () => {
         const y = Object.create({})
         const event = pick?.entry_history?.event
         const playerId = pick?.picks?.find(x => x.multiplier > 1)?.element
+        const multiplier = pick?.picks?.find(x => x.multiplier > 1)?.multiplier
         const web_name = players?.find(x => x.id === playerId)?.web_name
         const stats = playerStats.find(player => player.history[0].element === playerId)?.history.find(x => x.round === event)
         const teamId = players?.find(x => x.id === playerId)?.team
@@ -87,7 +131,7 @@ const AchievementsScreen = () => {
         //const elementTypeId = players?.find(x => x.id === playerId).element_type
         y.event = pick?.entry_history?.event
         y.captain = web_name
-        y.total_points = stats?.total_points
+        y.total_points = stats?.total_points*multiplier
         y.playerId = playerId
         y.stats = stats
         y.goals_scored = stats?.goals_scored
@@ -104,8 +148,6 @@ const AchievementsScreen = () => {
         y.team_a_score  = stats?.team_a_score
         y.team_h_score = stats?.team_h_score
         y.bonus = stats?.bonus
-  
-        
         return y
       })
       return a3
@@ -115,14 +157,14 @@ const AchievementsScreen = () => {
   
 
   const captainDetails = useMemo(() => 
-    getCaptains().filter(cap => (cap.total_points*2) >= value), [getCaptains, value])
+    getCaptains().filter(cap => (cap.total_points) >= value), [getCaptains, value])
 
-  const sixOrMore = getCaptains().filter(cap => (cap.total_points*2) >= 6)
-  const tenOrMore = getCaptains().filter(cap => (cap.total_points*2) >= 10)
-  const sixteenOrMore = getCaptains().filter(cap => (cap.total_points*2) >= 16)
-  const twentyOrMore = getCaptains().filter(cap => (cap.total_points*2) >= 20)
-  const thirtyOrMore = getCaptains().filter(cap => (cap.total_points*2) >= 30)
-  const thirtyFiveOrMore = getCaptains().filter(cap => (cap.total_points*2) >= 35)
+  const sixOrMore = getCaptains().filter(cap => (cap.total_points) >= 6)
+  const tenOrMore = getCaptains().filter(cap => (cap.total_points) >= 10)
+  const sixteenOrMore = getCaptains().filter(cap => (cap.total_points) >= 16)
+  const twentyOrMore = getCaptains().filter(cap => (cap.total_points) >= 20)
+  const thirtyOrMore = getCaptains().filter(cap => (cap.total_points) >= 30)
+  const thirtyFiveOrMore = getCaptains().filter(cap => (cap.total_points) >= 35)
 
   return (
     <>
@@ -166,6 +208,50 @@ const AchievementsScreen = () => {
               </div>
               <div onClick={handleTen} className="times">
               {tenOrMore.length > 0 ? tenOrMore.length : <BiLock />}
+              </div>
+            </div>
+            <div className="achieve-wrapper my-1">
+              <div className="achieve-details">
+                <div className="achieve-header">Semi Pro skipper</div>
+                <div className="achieve-note">
+                  Your captain scores more than 16 points
+                </div>
+              </div>
+              <div onClick={handleSixteen} className="times">
+              {sixteenOrMore.length > 0 ? sixteenOrMore.length : <BiLock />}
+              </div>
+            </div>
+            <div className="achieve-wrapper my-1">
+              <div className="achieve-details">
+                <div className="achieve-header">Pro skipper</div>
+                <div className="achieve-note">
+                  Your captain scores more than 20 points
+                </div>
+              </div>
+              <div onClick={handleTwenty} className="times">
+              {twentyOrMore.length > 0 ? twentyOrMore.length : <BiLock />}
+              </div>
+            </div>
+            <div className="achieve-wrapper my-1">
+              <div className="achieve-details">
+                <div className="achieve-header">WorldClass skipper</div>
+                <div className="achieve-note">
+                  Your captain scores more than 30 points
+                </div>
+              </div>
+              <div onClick={handleThirty} className="times">
+              {thirtyOrMore.length > 0 ? thirtyOrMore.length : <BiLock />}
+              </div>
+            </div>
+            <div className="achieve-wrapper my-1">
+              <div className="achieve-details">
+                <div className="achieve-header">Legendary skipper</div>
+                <div className="achieve-note">
+                  Your captain scores more than 35 points
+                </div>
+              </div>
+              <div onClick={handleThirtyFive} className="times">
+              {thirtyFiveOrMore.length > 0 ? thirtyFiveOrMore.length : <BiLock />}
               </div>
             </div>
           </div>
