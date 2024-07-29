65d25023e29b2e88dbe6b2a04290e194a77caa8e
Denis Moini <denismoini09@gmail.com>
2024-07-30 01:09:14 +0300

    added api proxy

--- src/screens/HomeScreen.jsx
+++ src/screens/HomeScreen.jsx
@@ -1,4 +1,6 @@
 import { Container } from "react-bootstrap"
+import {  Nav } from 'react-bootstrap'
+import { LinkContainer } from 'react-router-bootstrap'
 const HomeScreen = () => {
   return (
     <Container>
@@ -6,15 +8,19 @@ const HomeScreen = () => {
         <div className="home-pos">Welcome to the captain&lsquo;s lounge</div>
       </div>
       <div className="link-wrap py-3">
+      <LinkContainer to='/statistics'>
         <div className="pos-link position-relative">
           <div className="home-pos">Statistics</div>
         </div>
-        <div className="pos-link position-relative">
+        </LinkContainer>
+        <LinkContainer to='/compare'><div className="pos-link position-relative">
           <div className="home-pos">Comparison</div>
-        </div>
-        <div className="pos-link position-relative">
+        </div></LinkContainer>
+        
+        <LinkContainer to='/achievements'><div className="pos-link position-relative">
           <div className="home-pos">Achievements</div>
-        </div>
+        </div></LinkContainer>
+        
       </div>
     </Container>
   )
